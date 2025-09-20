151d11e989233eb0e53ffa064142b172e6a55a50
ckanayama <knym.777@gmail.com>
2025-09-20 23:13:39 +0900

    発音とコメントの調整

--- otenki/weather_logic.h
+++ otenki/weather_logic.h
@@ -5,27 +5,26 @@ String printWeatherAdvice(float tempMax, float tempMin, float precipitation) {
   String tempComment = "";
   String precipitationComment = "";
   
-  // 最高気温35℃以上
+  // 最高気温と最低気温(℃)
   if (tempMax >= 35.0) {
     tempComment = "kyo'-wa mousyobidesu. horeizaito,hi'gasawo,wasurezuni.";
-  }
-  // 最高気温30℃以上（猛暑日未満）
-  else if (tempMax >= 30.0) {
-    tempComment = "kyo'-wa manatubidesu horeizai,wo wasurezuni";
+  } else if (tempMax >= 30.0) {
+    tempComment = "kyo'-wa manatubidesu horeizai,wo wasurezuni.";
   } else if (tempMin <= 15.0) {
-    tempComment = "kyo'-wa hadazamuidesu uwagi,wo wasurezuni";
+    tempComment = "kyo'-wa hadazamuidesu uwagi,wo wasurezuni.";
   } else if (tempMin <= 10.0) {
-    tempComment = "kyo'-wa totemosamuidesu boukangi,wo wasurezuni";
+    tempComment = "kyo'-wa totemosamuidesu boukangi,wo wasurezuni.";
   } else {
     tempComment = "kyo'-wa sugosiyasui kiondesyou.";
   }
 
-  // 降水確率0%
-  if (precipitation <= 1.0) {
-    precipitationComment = "sentakuwa yo'kukawakudesyou.";
-  }
-  else {
-    precipitationComment = "sentakuwa heyabosi simasyou.";
+  // 降水量（mm）
+  if (precipitation <= 0) {
+    precipitationComment = "sentakuwa yo'kukawakudesyou";
+  } else if (precipitation <= 1.0) {
+    precipitationComment = "sentakuwa heyabosi,ka a'meni,kiwotukete";
+  } else {
+    precipitationComment = "sentakuwa heyabosi simasyou";
   }
 
   return tempComment + " " + precipitationComment;
