e2cc7a4997a9afaf87a4b0b1f8190baee4c81364
ckanayama <knym.777@gmail.com>
2025-09-20 22:52:50 +0900

    発音とコメントの調整

--- otenki/weather_logic.h
+++ otenki/weather_logic.h
@@ -7,22 +7,25 @@ String printWeatherAdvice(float tempMax, float tempMin, float precipitation) {
   
   // 最高気温35℃以上
   if (tempMax >= 35.0) {
-    tempComment = "kyouha mousyobi desu. horeizai to higasa wo wasurezuni.";
+    tempComment = "kyo'-wa mousyobidesu. horeizaito,hi'gasawo,wasurezuni.";
   }
   // 最高気温30℃以上（猛暑日未満）
   else if (tempMax >= 30.0) {
-    tempComment = "kyouha manatubi desu. horeizai wo wasurezuni.";
+    tempComment = "kyo'-wa manatubidesu horeizai,wo wasurezuni";
+  } else if (tempMin <= 15.0) {
+    tempComment = "kyo'-wa hadazamuidesu uwagi,wo wasurezuni";
+  } else if (tempMin <= 10.0) {
+    tempComment = "kyo'-wa totemosamuidesu boukangi,wo wasurezuni";
+  } else {
+    tempComment = "kyo'-wa sugosiyasui kiondesyou.";
   }
 
   // 降水確率0%
-  if (precipitation == 0.0) {
-    precipitationComment = "sentakuha yokukawakudesyou.";
-  }
-  else if (precipitation <= 1.0) {
-    precipitationComment = "sentakuha yokukawakudesyou.";
+  if (precipitation <= 1.0) {
+    precipitationComment = "sentakuwa yo'kukawakudesyou.";
   }
   else {
-    precipitationComment = "sentakuha heyabosi simasyou.";
+    precipitationComment = "sentakuwa heyabosi simasyou.";
   }
 
   return tempComment + " " + precipitationComment;
